2920f400d757bfaad5d5b049370e3db14402562b
GuruKR <gurukr033@gmail.com>
2026-08-10 15:42:10 +0530

    the g1 file is updated by master

--- g1.java
+++ g1.java
@@ -1,6 +1,7 @@
 public class g1 {
     public static void main(String[] args) {
         System.out.println("this is g1 file");
+        System.out.println("the g1 file is updated by the master");
     }
 
 }
